51dae020cb5dac6a5a620de147caa695320445e0
Shilpa Raveendran <Shilpa@10.0.0.56>
2023-10-02 17:32:16 -0400

    New codes to find duplicate elements

--- src/com/shilpa/DuplicateElements.java
+++ src/com/shilpa/DuplicateElements.java
@@ -1,9 +1,8 @@
 package com.shilpa;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class DuplicateElements {
     public static void main(String[] args) {
@@ -14,8 +13,40 @@ public class DuplicateElements {
         //output: 33,10,28
 
         Set<Integer> hs = new HashSet<>();
-        list.stream().filter(x -> !hs.add(x)).distinct().forEach(i->System.out.println(i));
+        //list.stream().filter(x -> !hs.add(x)).distinct().forEach(i->System.out.println(i));
+        Set<Integer> dupElemnt= list.stream().filter(x ->!hs.add(x)).collect(Collectors.toSet());
+        dupElemnt.forEach(x-> System.out.println(x));
+        System.out.println("=====Unique set=====");
+        hs.forEach(x-> System.out.println(x));
 
+
+        //finding it using map technique
+        // with key has integer and value as the frequency of occurrence
+
+        Map<Integer,Long> map=list.stream()
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+
+        System.out.println(map);
+        Set<Integer> dupSet= map.entrySet().stream()
+                .filter(entry -> entry.getValue() > 1)
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toSet());
+        System.out.println("====Collecting to a Set=====");
+        dupSet.forEach(x-> System.out.println(x));
+
+        System.out.println("====iterating through a map=====");
+        //Iterate through map and find the duplicates
+        for(Map.Entry<Integer,Long> entries : map.entrySet()){
+            if(entries.getValue() > 1){
+                System.out.println(entries.getKey());
+            }
+        }
+
+        //Using Collectors.frequency
+        System.out.println("====Using Collectors.frequency=====");
+        list.stream().filter(x -> Collections.frequency(list,x) >1)
+                .collect(Collectors.toSet())
+                .forEach( i-> System.out.println(i));
 
 
 //        Set<Integer> hs= new HashSet<>();
